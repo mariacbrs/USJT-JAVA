f315b29a0fa6d6568a1af2816a69068c5c97c5d1
Maria Clara Borges Rodrigues Santos <mariacbrsantos@gmail.com>
2024-04-16 10:46:35 -0300

    latest updates

--- eduSys/src/main/java/usjt/edusys/stuClass.java
+++ eduSys/src/main/java/usjt/edusys/stuClass.java
@@ -4,24 +4,25 @@ package usjt.edusys;
 import javax.swing.JOptionPane;
 
 public class stuClass {
-     String sName;
-     int sAge;
-     int sRA;
-     String sPhone;
-     String sEmail;
+     private String sName;
+     private int sAge;
+     private int sRA;
+     private String sPhone;
+     private String sEmail;
      private String sPass;
      
      
 //  METHODS:
-     public void identifyStu(){ 
-         JOptionPane.showMessageDialog(null, "Welcome student!"); 
+     public void identifyStu(){
+         setsName(JOptionPane.showInputDialog(null, "Your name:"));
+         JOptionPane.showMessageDialog(null, "Welcome student " + getsName() + "!"); 
          
-         sName =  JOptionPane.showInputDialog(null, "Your name:");
-         sAge = Integer.parseInt(JOptionPane.showInputDialog(null, "Your age:"));
-         sRA = Integer.parseInt(JOptionPane.showInputDialog(null, "Your RA:"));
-         sPhone = JOptionPane.showInputDialog(null, "Your phone:");
-         sEmail = JOptionPane.showInputDialog(null, "Your email:");
-         sPass = JOptionPane.showInputDialog(null, "Create a password:");
+         
+         setsAge(Integer.parseInt(JOptionPane.showInputDialog(null, "Your age:")));
+         setsRA(Integer.parseInt(JOptionPane.showInputDialog(null, "Your RA:")));
+         setsPhone(JOptionPane.showInputDialog(null, "Your phone:"));
+         setsEmail(JOptionPane.showInputDialog(null, "Your email:"));
+         setsPass(JOptionPane.showInputDialog(null, "Create a password:"));
          
            
      }
@@ -32,5 +33,89 @@ public class stuClass {
       public void viewCourseWork(){
          
      }
+
+    /**
+     * @return the sName
+     */
+    private String getsName() {
+        return sName;
+    }
+
+    /**
+     * @param sName the sName to set
+     */
+    private void setsName(String sName) {
+        this.sName = sName;
+    }
+
+    /**
+     * @return the sAge
+     */
+    private int getsAge() {
+        return sAge;
+    }
+
+    /**
+     * @param sAge the sAge to set
+     */
+    private void setsAge(int sAge) {
+        this.sAge = sAge;
+    }
+
+    /**
+     * @return the sRA
+     */
+    private int getsRA() {
+        return sRA;
+    }
+
+    /**
+     * @param sRA the sRA to set
+     */
+    private void setsRA(int sRA) {
+        this.sRA = sRA;
+    }
+
+    /**
+     * @return the sPhone
+     */
+    private String getsPhone() {
+        return sPhone;
+    }
+
+    /**
+     * @param sPhone the sPhone to set
+     */
+    private void setsPhone(String sPhone) {
+        this.sPhone = sPhone;
+    }
+
+    /**
+     * @return the sEmail
+     */
+    private String getsEmail() {
+        return sEmail;
+    }
+
+    /**
+     * @param sEmail the sEmail to set
+     */
+    private void setsEmail(String sEmail) {
+        this.sEmail = sEmail;
+    }
+
+    /**
+     * @return the sPass
+     */
+    private String getsPass() {
+        return sPass;
+    }
+
+    /**
+     * @param sPass the sPass to set
+     */
+    private void setsPass(String sPass) {
+        this.sPass = sPass;
+    }
      
 }
--- eduSys/src/main/java/usjt/edusys/teacherClass.java
+++ eduSys/src/main/java/usjt/edusys/teacherClass.java
@@ -4,20 +4,21 @@ package usjt.edusys;
 import javax.swing.JOptionPane;
 
 public class teacherClass {
-    String tName;
-    int tAge;
-    String tEmail;
+    private String tName;
+    private int tAge;
+    private String tEmail;
     private String tPass;
    
     
 // METHODS:    
 
    public void identifyTeacher(){
-        JOptionPane.showMessageDialog(null, "Welcome teacher!");
-        tName = JOptionPane.showInputDialog(null, "Your name:");
-        tAge = Integer.parseInt(JOptionPane.showInputDialog(null, "Your age:"));
-        tEmail = JOptionPane.showInputDialog(null, "Your email:");
-        tPass = JOptionPane.showInputDialog(null, "Create a password:");
+        settName(JOptionPane.showInputDialog(null, "Your name:"));
+        JOptionPane.showMessageDialog(null, "Welcome teacher " + gettName() + "!");
+        
+        settAge(Integer.parseInt(JOptionPane.showInputDialog(null, "Your age:")));
+        settEmail(JOptionPane.showInputDialog(null, "Your email:"));
+        settPass(JOptionPane.showInputDialog(null, "Create a password:"));
         
    } 
    public void addGrades(){
@@ -32,13 +33,69 @@ public class teacherClass {
           //The loop for is going through the entire lenght of the array allowing the teacher to insert the grades and calculate the avarage for each student
           for(int i=0; i< student.length; i++ ){
               
-             double a1 = Double.parseDouble(JOptionPane.showInputDialog(null, "insert A1 grade: "));
-             double a2 = Double.parseDouble(JOptionPane.showInputDialog(null, "insert A2 grade: "));
-             double a3 = Double.parseDouble(JOptionPane.showInputDialog(null, "insert A3 grade: "));  
+             double a1 = Double.parseDouble(JOptionPane.showInputDialog(null, "insert A1 grade of the student " + student[i]));
+             double a2 = Double.parseDouble(JOptionPane.showInputDialog(null, "insert A2 grade of the student " + student[i]));
+             double a3 = Double.parseDouble(JOptionPane.showInputDialog(null, "insert A3 grade of the student " + student[i]));  
            
              double avg = (a1+a2+a3) / 3;
              JOptionPane.showMessageDialog(null, " " + student[i]+ "'s avarage in the class is " + avg);
              
           }
    }
+
+    /**
+     * @return the tName
+     */
+    private String gettName() {
+        return tName;
+    }
+
+    /**
+     * @param tName the tName to set
+     */
+    private void settName(String tName) {
+        this.tName = tName;
+    }
+
+    /**
+     * @return the tAge
+     */
+    private int gettAge() {
+        return tAge;
+    }
+
+    /**
+     * @param tAge the tAge to set
+     */
+    private void settAge(int tAge) {
+        this.tAge = tAge;
+    }
+
+    /**
+     * @return the tEmail
+     */
+    private String gettEmail() {
+        return tEmail;
+    }
+
+    /**
+     * @param tEmail the tEmail to set
+     */
+    private void settEmail(String tEmail) {
+        this.tEmail = tEmail;
+    }
+
+    /**
+     * @return the tPass
+     */
+    private String gettPass() {
+        return tPass;
+    }
+
+    /**
+     * @param tPass the tPass to set
+     */
+    private void settPass(String tPass) {
+        this.tPass = tPass;
+    }
 }
